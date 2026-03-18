57558c3abf3381daafdc2f356a3c6590dbdec44a
Arch-Tom <tom@archengraving.com>
2026-03-18 15:13:27 -0500

    More updates to Live Preview for visual fixes

--- src/components/livePreview/PreviewRangeControl.jsx
+++ src/components/livePreview/PreviewRangeControl.jsx
@@ -45,15 +45,17 @@ const PreviewRangeControl = ({
                 </label>
 
                 <div className="flex items-center gap-2">
-                    <span className="inline-flex min-w-[3rem] items-center justify-center rounded-full border border-slate-200/90 bg-white/95 px-2.5 py-1 text-xs font-semibold text-slate-700 shadow-sm">
-                        {displayValue}
-                    </span>
+                    {!showNumberInput && (
+                        <span className="inline-flex min-w-[3rem] items-center justify-center rounded-full border border-slate-200/90 bg-white/95 px-2.5 py-1 text-xs font-semibold text-slate-700 shadow-sm">
+                            {displayValue}
+                        </span>
+                    )}
                     {secondaryAction}
                 </div>
             </div>
 
             <div className="space-y-3">
-                <div className="relative">
+                <div className="relative py-1">
                     <div className="pointer-events-none absolute inset-x-0 top-1/2 h-2 -translate-y-1/2 rounded-full bg-slate-200" />
                     <div
                         className="pointer-events-none absolute left-0 top-1/2 h-2 -translate-y-1/2 rounded-full bg-[linear-gradient(90deg,rgba(59,130,246,0.95),rgba(96,165,250,0.9))]"
@@ -67,35 +69,15 @@ const PreviewRangeControl = ({
                         step={step}
                         value={value}
                         onChange={onChange}
-                        className="relative z-10 h-6 w-full cursor-pointer appearance-none bg-transparent
-                            [&::-webkit-slider-runnable-track]:h-2
-                            [&::-webkit-slider-runnable-track]:rounded-full
-                            [&::-webkit-slider-runnable-track]:bg-transparent
-                            [&::-webkit-slider-thumb]:mt-[-4px]
-                            [&::-webkit-slider-thumb]:h-4
-                            [&::-webkit-slider-thumb]:w-4
-                            [&::-webkit-slider-thumb]:appearance-none
-                            [&::-webkit-slider-thumb]:rounded-full
-                            [&::-webkit-slider-thumb]:border-2
-                            [&::-webkit-slider-thumb]:border-white
-                            [&::-webkit-slider-thumb]:bg-blue-600
-                            [&::-webkit-slider-thumb]:shadow-[0_8px_18px_-8px_rgba(37,99,235,0.65)]
-                            [&::-moz-range-track]:h-2
-                            [&::-moz-range-track]:rounded-full
-                            [&::-moz-range-track]:bg-transparent
-                            [&::-moz-range-thumb]:h-4
-                            [&::-moz-range-thumb]:w-4
-                            [&::-moz-range-thumb]:appearance-none
-                            [&::-moz-range-thumb]:rounded-full
-                            [&::-moz-range-thumb]:border-2
-                            [&::-moz-range-thumb]:border-white
-                            [&::-moz-range-thumb]:bg-blue-600
-                            [&::-moz-range-thumb]:shadow-[0_8px_18px_-8px_rgba(37,99,235,0.65)]"
+                        className="preview-range relative z-10"
                     />
                 </div>
 
                 {showNumberInput && (
-                    <div className="flex justify-end">
+                    <div className="flex items-center justify-between gap-3">
+                        <span className="text-xs font-medium text-slate-400">
+                            Current value
+                        </span>
                         <input
                             type="number"
                             min={min}
